4cdf3c6df6d62836332a1a530b4d2422996a56f2
Pavel Obrtlik <p.obrtlik@gmail.com>
2015-02-17 07:21:01 +0100

    -- převedení routování z RouteFactory do bootstrap.php

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -1,5 +1,5 @@
 <?php
-
+use \Nette\Application\Routers\Route as Route;
 require __DIR__ . '/../vendor/autoload.php';
 
 $configurator = new Nette\Configurator;
@@ -17,5 +17,31 @@ $configurator->addConfig(__DIR__ . '/config/config.neon');
 $configurator->addConfig(__DIR__ . '/config/config.local.neon');
 
 $container = $configurator->createContainer();
+
+$container->router[] = new \Nette\Application\Routers\Route('produkty/<id>', array(
+			'id' => array(
+				Route::FILTER_IN => function ($id) use ($container) {
+					if(is_numeric($id)) {
+						return $id;
+					} else {
+						/** @var $products Nette\Database\Table\Selection */
+						$products = $container->createProducts();
+						return $products->where('uri', $id)->fetch()->id;
+					}
+				},
+				Route::FILTER_OUT => function ($id) use ($container) {
+					if (!is_numeric($id)) {
+						return $id;
+					} else {
+						/** @var $pages Nette\Database\Table\Selection */
+						$pages = $container->createProducts();
+					}
+				}
+			),
+			'presenter' => 'Products',
+			'action' => 'product'
+		));
+$container->router[] = new Route('<presenter>/<action>[/<id>]', 'Homepage:default');
+$container->router[] = new Route('<presenter>/<action>/<id>', 'Homepage:default');
 
 return $container;
